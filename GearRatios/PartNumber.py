497fb61f2773f0d677d43a461f776b82461c92e2
Michael Westerman <michaeldavid.westerman@draeger.com>
2024-11-06 16:42:08 +0200

    Use dataclass

--- GearRatios/PartNumber.py
+++ GearRatios/PartNumber.py
@@ -1,5 +1,8 @@
+from dataclasses import dataclass
+
+@dataclass
 class PartNumber:
-    def __init__(self, value: int, start_index: int, end_index: int):
-        self.value = value
-        self.start_index = start_index
-        self.end_index = end_index
+    value: int
+    start_index: int
+    end_index: int
+
